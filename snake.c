dbc1a056af9088d15cdf9fe3a18311cea9526e9b
isabella marques <isabella>
2026-01-16 01:55:40 -0300

    read_keyboard func

--- snake.c
+++ snake.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <conio.h>
 
-#define cols 20 
+#define cols 50
 #define rows 20
 
 char board[cols * rows];
@@ -17,12 +18,48 @@ void fill_board()
             {
                 board[y * cols + x] = '#';
             }
+            else
+            {
+                board[y * cols + x] = ' ';
+            }
         }   
+    }
+}
+
+void print_board()
+{
+    int y,x;
+
+    for(y = 0; y < rows; y++)
+    {
+        for(x =0; x < cols; x++)
+        {
+            putch(board[y * cols + x]);
+        }   
+        putch('\n');
+    }
+}
+
+void read_keyboard()
+{
+
+    int ch = getch();
+
+    switch (ch)
+    {
+    case 'w': move_snake(0, -1); break;
+    case 's': move_snake(0, 1); break;
+    case 'a': move_snake(-1, 0); break;
+    case 'd': move_snake(1, 0); break;
     }
 }
 
 int main(int argc, char **argv)
 {
+    fill_board();
+    print_board();
+
+    read_keyboard();
 
     return 0; 
 }
